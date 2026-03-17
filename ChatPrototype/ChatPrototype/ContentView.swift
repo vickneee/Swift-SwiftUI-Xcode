1744dede98cbad1fd264bc8d4b08afb391c8ef8c
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-03-17 12:26:02 +0200

    Add padding, background

--- ChatPrototype/ChatPrototype/ContentView.swift
+++ ChatPrototype/ChatPrototype/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Knock, knock!")
+                .padding(16)
+                .background(Color.yellow, in: RoundedRectangle (cornerRadius: 10))
+            Text("Whos's there?")
+                .padding(16)
+                .background(Color.teal, in: RoundedRectangle (cornerRadius: 10))
         }
         .padding()
     }
