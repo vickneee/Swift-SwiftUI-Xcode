eeda65036c3b6a6498b3719c200308b13ca3a2d8
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-03-29 04:39:28 +0300

    Initial commit

---
+++ Pick-a-Pal/Pick-a-Pal/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  Pick-a-Pal
+//
+//  Created by Victoria Vavulina on 29.3.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var names: [String] = ["Elisha", "Andre", "Jasmine", "Po-Chun"]
+    @State private var nameToAdd: String = ""
+    @State private var pickedName = ""
+    @State private var shouldRemovePickedName = false
+    
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                Text("Pick a Pal")
+            }
+            .font(.title)
+            .bold()
+            
+            Text(pickedName.isEmpty ? " " : pickedName)
+                .font(.title2)
+                .bold()
+                .foregroundStyle(.tint)
+                
+            List {
+                ForEach(names, id: \.description) { name in
+                    Text(name)
+                }
+            }
+            TextField("Add name", text: $nameToAdd)
+                .onSubmit {
+                    if !nameToAdd.isEmpty {
+                        names.append(nameToAdd)
+                        nameToAdd = ""
+                    }
+                }
+            
+            Divider()
+            
+            Toggle("Remove when picked", isOn: $shouldRemovePickedName)
+            
+            Button {
+                if let randomName = names.randomElement() {
+                    pickedName = randomName
+                    
+                    if shouldRemovePickedName {
+                        names.removeAll { name in
+                            return (name == randomName)
+                        }
+                    }
+                } else {
+                    pickedName = ""
+                }
+            } label: {
+                Text("Pick Random Name")
+                    .font(.title2)
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 16)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle(radius: 8))
+            .font(.title2)
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Victoria Vavulina on 29.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
