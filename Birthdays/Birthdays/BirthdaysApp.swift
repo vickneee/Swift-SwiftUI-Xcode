561b7f231bd0871334e68dcab81e7cda3d5d48de
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-03-31 02:29:36 +0300

    Initial commit

---
+++ Birthdays/Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Victoria Vavulina on 30.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birthdays/Birthdays/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Birthdays
+//
+//  Created by Victoria Vavulina on 30.3.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
