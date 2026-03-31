37082b3482830111d6bde1977330d4e0221ce9e2
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-03-31 05:24:21 +0300

    Initial commit

---
+++ ScoreKeeper/ScoreKeeper/Player.swift
@@ -0,0 +1,8 @@
+//
+//  Player.swift
+//  ScoreKeeper
+//
+//  Created by Victoria Vavulina on 31.3.2026.
+//
+
+import Foundation
---
+++ ScoreKeeper/ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Victoria Vavulina on 31.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
